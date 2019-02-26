6c2e0b4651abf755b592362abb1260d168352087
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-26 03:02:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ class Passenger{
     trips(){
       return store.trips.filter(
         function(trip){
-          return trip.driverId === this.id;
+          return trip.passengerId === this.id;
         }.bind(this)
       )
     }
